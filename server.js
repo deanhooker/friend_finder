92c69b762b7c9bd6063c9c1402e911c913b33a06
Dean Hooker <deanhooker91@gmail.com>
2018-02-16 19:01:21 -0700

    server.js set up

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// Create express server
+var app = express();
+// Set port
+var PORT = 3000;
+
+// Handle data parsing
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+});
