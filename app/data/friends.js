23d0c6ab76ae01af78241ea53ecf28611da63d41
Dean Hooker <deanhooker91@gmail.com>
2018-02-17 11:13:51 -0700

    set up api routing

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+let friendsDataArray = [
+    {
+        name: "The Dude",
+        photo: "https://coedmagazine.files.wordpress.com/2016/09/dude.jpg?quality=88&w=750&h=422",
+        answers: [5, 5, 4, 2, 1, 3, 4, 2, 4, 1]
+    }
+]
+
+module.exports = friendsDataArray;
