ddd3d750ec0ac85e14bbab86a819304cd0a1ef0b
Dean Hooker <deanhooker91@gmail.com>
2018-02-17 17:46:55 -0700

    added questions and characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,69 @@
+// Character set up
+
+// Question         |   1 2 3 4 5 6 7 8 9 10
+// ----------------------------------
+// The Dude         |   1 3 5 5 5 4 2 5 5 1
+// Luke Skywalker   |   5 4 3 2 4 2 2 2 4 2
+// Steve Zissou     |   3 2 4 2 2 4 2 2 2 2
+// Jack Sparrow     |   4 3 3 1 4 5 2 1 3 5
+// Tony Stark       |   5 1 5 1 2 5 1 3 1 2
+// John McClane     |   2 4 5 4 4 3 4 5 3 1
+// Dr. Emmett Brown |   5 1 4 1 4 5 5 1 1 1
+// Rocky Balboa     |   2 2 4 5 2 3 2 2 4 4
+// Fogell/McLovin   |   4 2 1 3 2 2 5 1 2 5
+// Indiana Jones    |   5 5 3 2 4 2 4 3 2 3
+
+
 let friendsDataArray = [
     {
         name: "The Dude",
         photo: "https://coedmagazine.files.wordpress.com/2016/09/dude.jpg?quality=88&w=750&h=422",
-        answers: [5, 5, 4, 2, 1, 3, 4, 2, 4, 1]
+        answers: [1, 3, 5, 5, 5, 4, 2, 5, 5, 1]
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "",
+        answers: [5, 4, 3, 2, 4, 2, 2, 2, 4, 2]
+    },
+    {
+        name: "Steve Zissou",
+        photo: "",
+        answers: [3, 2, 4, 2, 2, 4, 2, 2, 2, 2,]
+    },
+    {
+        name: "Jack Sparrow",
+        photo: "",
+        answers: [4, 3, 3, 1, 4, 5, 2, 1, 3, 5]
+    },
+    {
+        name: "Tony Stark",
+        photo: "",
+        answers: [5, 1, 5, 1, 2, 5, 1, 3, 1, 2]
+    },
+    {
+        name: "John McClane",
+        photo: "",
+        answers: [2, 4, 5, 4, 4, 3, 4, 5, 3, 1]
+    },
+    {
+        name: "Dr. Emmett Brown",
+        photo: "",
+        answers: [5, 1, 4, 1, 4, 5, 5, 1, 1, 1]
+    },
+    {
+        name: "Rocky Balboa",
+        photo: "",
+        answers: [2, 2, 4, 5, 2, 3, 2, 2, 4, 4]
+    },
+    {
+        name: "Fogell aka 'McLovin'",
+        photo: "",
+        answers: [4, 2, 1, 3, 2, 2, 5, 1, 2, 5]
+    },
+    {
+        name: "Indiana Jones",
+        photo: "",
+        answers: [5, 5, 3, 2, 4, 2, 4, 3, 2, 3]
     }
 ]
 
